b6abc9b24848444c2b80be664950fedce35e439a
pteyssedre <p0l1c3$974zaza>
2016-06-24 21:52:13 -0400

    Fixing test Creation Test Module

--- test/test.js
+++ test/test.js
@@ -1,103 +1,142 @@
 var chai = require("chai");
 var expect = chai.expect;
 var LazyUAC = require('../dist/src/uac').LazyUAC;
-var DataService = require('../dist/src/service/data.service').DataService;
 var DataModel = require('../dist/src/model/models').DataModel;
 
+//noinspection JSUnresolvedFunction
+describe('Module', function () {
+    //noinspection JSUnresolvedFunction
+    describe('DataModel', function () {
 
-describe('DataModel', function () {
-
-    describe('UserModel test', function () {
-        it('Should create an User and encrypt a Password in async mode', function (done) {
-            var user = new DataModel.User("Pierre", "Teyssedre", "pierre@teyssedre.ca");
-            user.AddPassword("chiendechasse", function () {
-                expect(user.Password).to.not.equal("chiendechasse");
-                done();
-            });
-        });
-        it('Should create an User and encrypt a Password in sync mode', function () {
-            var user = new DataModel.User("Pierre", "Teyssedre", "pierre@teyssedre.ca");
-            user.AddPasswordSync("chiendechasse");
-            expect(user.Password).to.not.equal("chiendechasse");
-        });
-        it('Should return true on password compare in async mode', function () {
-            var user = new DataModel.User("Pierre", "Teyssedre", "pierre@teyssedre.ca");
-            user.AddPassword("chiendechasse", function (error, result) {
-                expect(user.Password).to.not.equal("chiendechasse");
-                user.ComparePassword("chiendechasse", function (match) {
-                    expect(match).to.equal(true);
+        //noinspection JSUnresolvedFunction
+        describe('UserModel test', function () {
+            //noinspection JSUnresolvedFunction
+            it('Should create an User and encrypt a Password in async mode', function (done) {
+                var user = new DataModel.User("Pierre", "Teyssedre", "pierre@teyssedre.ca");
+                user.AddPassword("chiendechasse", function () {
+                    //noinspection JSUnresolvedVariable
+                    expect(user.Password).to.not.equal("chiendechasse");
+                    //noinspection JSUnresolvedVariable
+                    expect(user.ComparePasswordSync("chiendechasse")).to.equal(true);
+                    done();
                 });
             });
+            //noinspection JSUnresolvedFunction
+            it('Should create an User and encrypt a Password in sync mode', function () {
+                var user = new DataModel.User("Pierre", "Teyssedre", "pierre@teyssedre.ca");
+                user.AddPasswordSync("chiendechasse");
+                //noinspection JSUnresolvedVariable
+                expect(user.Password).to.not.equal("chiendechasse");
+                //noinspection JSUnresolvedVariable
+                expect(user.ComparePasswordSync("chiendechasse")).to.equal(true);
+            });
+            //noinspection JSUnresolvedFunction
+            it('Should return true on password compare in async mode', function () {
+                var user = new DataModel.User("Pierre", "Teyssedre", "pierre@teyssedre.ca");
+                user.AddPassword("chiendechasse", function () {
+                    //noinspection JSUnresolvedVariable
+                    expect(user.Password).to.not.equal("chiendechasse");
+                    user.ComparePassword("chiendechasse", function (match) {
+                        //noinspection JSUnresolvedVariable
+                        expect(match).to.equal(true);
+                    });
+                });
+            });
+            //noinspection JSUnresolvedFunction
+            it('Should return true on password compare in sync mode', function () {
+                var user = new DataModel.User("Pierre", "Teyssedre", "pierre@teyssedre.ca");
+                user.AddPasswordSync("chiendechasse");
+                //noinspection JSUnresolvedVariable
+                expect(user.Password).to.not.equal("chiendechasse");
+                var match = user.ComparePasswordSync("chiendechasse");
+                //noinspection JSUnresolvedVariable
+                expect(match).to.equal(true);
+            });
+            //noinspection JSUnresolvedFunction
+            it('Should create two user using the same password but the hash should not be equal', function () {
+                var user = new DataModel.User("Pierre", "Teyssedre", "pierre@teyssedre.ca");
+                var user2 = new DataModel.User("Pierre2", "Teyssedre2", "pierre@teyssedre.ca");
+                user.AddPasswordSync("chiendechasse");
+                user2.AddPasswordSync("chiendechasse");
+                //noinspection JSUnresolvedVariable
+                expect(user.Password).to.not.equal(user2.Password);
+            });
         });
-        it('Should return true on password compare in sync mode', function () {
-            var user = new DataModel.User("Pierre", "Teyssedre", "pierre@teyssedre.ca");
-            user.AddPasswordSync("chiendechasse");
-            expect(user.Password).to.not.equal("chiendechasse");
-            var match = user.ComparePasswordSync("chiendechasse");
-            expect(match).to.equal(true);
-        });
-        it('Should create two user using the same password but the hash should not be equal', function () {
-            var user = new DataModel.User("Pierre", "Teyssedre", "pierre@teyssedre.ca");
-            var user2 = new DataModel.User("Pierre2", "Teyssedre2", "pierre@teyssedre.ca");
-            user.AddPasswordSync("chiendechasse");
-            user2.AddPasswordSync("chiendechasse");
-            expect(user.Password).to.not.equal(user2.Password);
+        //noinspection JSUnresolvedFunction
+        describe('ProfileModel test', function () {
+            //noinspection JSUnresolvedFunction
+            it('Should create a profile', function () {
+                var user = new DataModel.User("Pierre", "Teyssedre", "pierre@teyssedre.ca");
+                var profile = new DataModel.Profile(user);
+                //noinspection JSUnresolvedVariable
+                expect(profile).to.not.equal(null);
+                //noinspection JSUnresolvedVariable
+                expect(profile.UserId).to.equal(user.Id);
+            });
         });
     });
-    describe('ProfileModel test', function () {
-        it('Should create a profile', function () {
-            var user = new DataModel.User("Pierre", "Teyssedre", "pierre@teyssedre.ca");
-            var profile = new DataModel.Profile(user);
-            expect(profile).to.not.equal(null);
-            expect(profile.UserId).to.equal(user.Id);
-        });
-    });
-});
-describe('lazyUAC', function () {
+    //noinspection JSUnresolvedFunction
+    describe('lazyUAC', function () {
 
-    var mockPassword = "Reacts987";
-    var mockUser = new DataModel.User("Pierre",
-        "Teyssedre",
-        "pierre@teyssedre.ca",
-        mockPassword,
-        DataModel.Role.VIEWER | DataModel.Role.USER
-    );
-    var uac;
+        var mockPassword = "Reacts987";
+        var mockUser = new DataModel.User("Pierre",
+            "Teyssedre",
+            "pierre@teyssedre.ca",
+            mockPassword,
+            DataModel.Role.VIEWER | DataModel.Role.USER
+        );
+        var uac;
 
-    describe('Default options test', function () {
-        it('Should create UAC databases and connect to them', function (done) {
-            uac = new LazyUAC.UserManager();
-            expect(uac).to.not.equal(null);
-            uac.StartManager(function (error, result) {
-                expect(error).to.equal(null);
-                expect(result).to.not.equal(null);
-                done();
+        //noinspection JSUnresolvedFunction
+        describe('Default options test', function () {
+            //noinspection JSUnresolvedFunction
+            it('Should create UAC databases and connect to them', function (done) {
+                uac = new LazyUAC.UserManager();
+                //noinspection JSUnresolvedVariable
+                expect(uac).to.not.equal(null);
+                uac.StartManager(function (error, result) {
+                    //noinspection JSUnresolvedVariable
+                    expect(error).to.equal(null);
+                    //noinspection JSUnresolvedVariable
+                    expect(result).to.not.equal(null);
+                    done();
+                });
             });
-        });
-        it('Should create an User and insert it inside the db', function (done) {
-            uac.AddUser(mockUser, function (result) {
-                expect(result).to.not.equal(null);
-                expect(result.Id).to.equal(mockUser.Id);
-                done();
+            //noinspection JSUnresolvedFunction
+            it('Should create an User and insert it inside the db', function (done) {
+                uac.AddUser(mockUser, function (result) {
+                    //noinspection JSUnresolvedVariable
+                    expect(result).to.not.equal(null);
+                    //noinspection JSUnresolvedVariable
+                    expect(result.Id).to.equal(mockUser.Id);
+                    done();
+                });
             });
-        });
-        it('Should validate the insertion of the user in the db', function (done) {
-            uac.GetUserByUserName(mockUser.Email, function (user) {
-                expect(user).to.not.equal(null);
-                expect(user.Id).to.equal(mockUser.Id);
-                done();
+            //noinspection JSUnresolvedFunction
+            it('Should validate the insertion of the user in the db', function (done) {
+                uac.GetUserByUserName(mockUser.Email, function (user) {
+                    //noinspection JSUnresolvedVariable
+                    expect(user).to.not.equal(null);
+                    //noinspection JSUnresolvedVariable
+                    expect(user.Id).to.equal(mockUser.Id);
+                    done();
+                });
             });
-        });
-        it('Should authenticated the user and return a object', function (done) {
-            uac.Authenticate(mockUser.Email, mockPassword, function (authenticated) {
-                expect(authenticated).to.equal(true);
-                done();
+            //noinspection JSUnresolvedFunction
+            it('Should authenticated the user and return a object', function (done) {
+                uac.Authenticate(mockUser.Email, mockPassword, function (authenticated) {
+                    //noinspection JSUnresolvedVariable
+                    expect(authenticated).to.equal(true);
+                    done();
+                });
             });
-        });
-        it('Should authenticated the user and return a object', function (done) {
-            uac.DeleteUser(mockUser.Id, function (deleted) {
-                expect(deleted).to.equal(true);
-                done();
+            //noinspection JSUnresolvedFunction
+            it('Should authenticated the user and return a object', function (done) {
+                uac.DeleteUser(mockUser.Id, function (deleted) {
+                    //noinspection JSUnresolvedVariable
+                    expect(deleted).to.equal(true);
+                    done();
+                });
             });
         });
     });
